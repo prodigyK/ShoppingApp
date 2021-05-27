d79a995e940b0e8ed78755768fdee8434a701105
prodigyK <kkpetkov@gmail.com>
2021-05-27 15:44:49 +0300

    Add empty cart

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,8 +1,12 @@
+import 'package:course_firebase_app/screens/cart_empty.dart';
 import 'package:flutter/material.dart';
 
 class Cart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Cart'));
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: CartEmpty(size: size),
+    );
   }
 }
